3e562c2ffaeedb642cce3875ef24bece767c1820
YoYoZ <yoyoza5@gmail.com>
2017-04-11 20:47:20 +0300

    Initial Commit

---
+++ lab4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab4
+//
+//  Created by Anton Solyarik on 11.04.17.
+//  Copyright © 2017 Anton Solyarik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
