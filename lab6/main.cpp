11faa23c27e6bdf64d27f2b69283474e69cc7242
YoYoZ <yoyoza5@gmail.com>
2017-05-24 13:00:30 +0300

    Updates

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -1,15 +1,51 @@
 //
 //  main.cpp
-//  lab6
+//  lab5
 //
-//  Created by Anton Solyarik on 15.05.17.
+//  Created by Anton Solyarik on 14.05.17.
 //  Copyright © 2017 Anton Solyarik. All rights reserved.
 //
 
 #include <iostream>
+#include <deque>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cout<<"enter selection: 1 - int, 2 - float, 3 - double"<<endl;
+    int sel;
+    cin>>sel;
+    switch (sel)
+    {
+        case 1: {
+            deque<int> d;
+            for (int i = 1; i <= 10; ++i) {
+                d.push_front(i);
+                cout<<"Now putting "<<i<<endl;
+                
+            }
+        }
+            break;
+        case 2: {
+            deque<float> e;
+            for (float i = 1; i <= 10; i=i+1.25) {
+                e.push_front(i);
+                cout<<"Now putting "<<i<<endl;
+                
+            }
+        }
+            break;
+        case 3: {
+            deque<double> z;
+            for (double i = 1; i <= 10; i=i+1.25) {
+                z.push_front(i);
+                cout<<"Now putting "<<i<<endl;
+                
+            }
+        }
+            break;
+    }
+    
+    
     return 0;
 }
